d102d5df051e2e1b2f5fd7e4ca35ff9900eec1ab
nachoto <joseignacio.rendofernandez@gmail.com>
2014-09-21 19:37:50 +0200

    .

---
+++ download_file.R
@@ -0,0 +1,5 @@
+# load the file
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, destfile="./data_project.zip", method="curl")
+
+# unzip manually the file.
